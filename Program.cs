aa8622004be473ee4bb7344a39a43db4ebfde098
dyaz1337 <112968308+dyaz1337@users.noreply.github.com>
2023-01-22 20:19:02 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int Stepen(int a, int b)
+// {
+//   int result = 1;
+//   for(int i = 1; i <= b; i++)
+//   {
+//     result = result * a;
+//   }
+//     return result;
+// }
+// Console.WriteLine("Ответ: " + Stepen(a, b));
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.Write("Введите первое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// int [] array = new int [8];
+// Console.Write("[");
+// for (int i = 0; i < array.Length; i++)
+// {
+//  array[i] = new Random().Next(0, 11);
+//  Console.Write(array [i] + ",");
+// }
+// Console.Write("]");
